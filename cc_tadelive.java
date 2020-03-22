99918976ca17f6b64d9369c1acb483998ef639fb
shobhitkrsks <sks.shobhit12@gmail.com>
2020-03-22 18:16:07 +0530

    tadelive accepted

--- cc_tadelive.java
+++ cc_tadelive.java
@@ -1,40 +1,11 @@
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.lang.Math;
+import java.util.*;
 
 public class cc_tadelive
 {
-
-    public static HashMap<Integer,Integer> sortmap(HashMap<Integer,Integer> hmap)
-    {
-        List<Map.Entry<Integer,Integer>> list=new LinkedList<Map.Entry<Integer,Integer>>(hmap.entrySet());
-
-        Collections.sort(list,new Comparator<Map.Entry<Integer,Integer>>()
-        {
-            public int compare(Map.Entry<Integer,Integer> o1, Map.Entry<Integer,Integer> o2)
-            {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
-
-        HashMap<Integer,Integer> temp=new LinkedHashMap<>();
-        for(Map.Entry<Integer,Integer> e:list)
-        {
-            temp.put(e.getKey(),e.getValue());
-        }
-
-        return temp;
-    }
-
-    public static void main(String[] args)
-    {
-        Scanner sc=new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int x=sc.nextInt();
         int y=sc.nextInt();
@@ -42,6 +13,8 @@ public class cc_tadelive
         
         HashMap<Integer,Integer> ma=new HashMap<>();
         HashMap<Integer,Integer> mb=new HashMap<>();
+        HashMap<Integer,Integer> diff=new HashMap<>();
+
 
         for(int i=0;i<n;i++)
         {
@@ -53,19 +26,69 @@ public class cc_tadelive
         {
             t=sc.nextInt();
             mb.put(i,t);
+            diff.put(i, Math.abs(ma.get(i)-t));
         }
 
-        ma=sortmap(ma);
-        mb=sortmap(mb);
+        diff=sortrevmap(diff);
 
-        for(Map.Entry<Integer,Integer> m:ma.entrySet())
-        System.out.println(m.getKey()+"->"+m.getValue());
+        //for(Map.Entry<Integer,Integer> m:diff.entrySet())
+        //System.out.println(m.getKey()+"->"+m.getValue());
 
-        for(Map.Entry<Integer,Integer> m:mb.entrySet())
-        System.out.println(m.getKey()+"->"+m.getValue());
-
-
-
+        long sum=0;
+        int a=0,b=0;
+        for(Map.Entry<Integer,Integer> m:diff.entrySet())
+        {
+            if(ma.get(m.getKey())>=mb.get(m.getKey()))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+            {
+                if(a<x)
+                {
+                    sum+=ma.get(m.getKey());
+                    a++;
+                }
+                else
+                {
+                    sum+=mb.get(m.getKey());
+                    b++;
+                }
+            }
+            else
+            {
+                if(b<y)
+                {
+                    sum+=mb.get(m.getKey());
+                    b++;
+                }
+                else
+                {
+                    sum+=ma.get(m.getKey());
+                    a++;
+                }
+            }
+        }
+        System.out.println(sum);
         sc.close();
+	}
+	
+	public static HashMap<Integer,Integer> sortrevmap(HashMap<Integer,Integer> hmap)
+    {
+        List<Map.Entry<Integer,Integer>> list=new LinkedList<Map.Entry<Integer,Integer>>(hmap.entrySet());
+
+        Collections.sort(list,new Comparator<Map.Entry<Integer,Integer>>()
+        {
+            public int compare(Map.Entry<Integer,Integer> o1, Map.Entry<Integer,Integer> o2)
+            {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+
+        Collections.reverse(list);
+
+        HashMap<Integer,Integer> temp=new LinkedHashMap<>();
+        for(Map.Entry<Integer,Integer> e:list)
+        {
+            temp.put(e.getKey(),e.getValue());
+        }
+
+        return temp;
     }
 }
